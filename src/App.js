f928b9e0febd46631492ba2c312519804cdd5434
Rakib Uddin <16103030@iubat.edu>
2019-05-22 23:00:46 +0600

    First Page Design Complete

--- src/App.js
+++ src/App.js
@@ -5,8 +5,34 @@ function App() {
   return (
     <div className="App">
       <div className="multipageForm">
-        <div className="pages">Form</div>
-        <div className="pages">Real Time Update</div>
+        <div className="pages">
+          <h3>Your Personal Information</h3>
+          <div className="fields">
+            <p>First Name</p>
+            <input 
+              placeholder="Enter Your First Name"
+            />
+          </div>
+          <div className="fields">
+            <p>Middle Name</p>
+            <input 
+              placeholder="Enter Your Middle Name"
+            />
+          </div>
+          <div className="fields">
+            <p>Last Name</p>
+            <input 
+              placeholder="Enter Your Last Name"
+            />
+          </div>
+          <div className="buttonContainer">
+            <button><i class="fas fa-angle-left"></i>Prev</button>
+            <button>Next<i class="fas fa-angle-right"></i></button>
+          </div>
+        </div>
+        <div className="pages">
+          <h3>Real Time Update</h3>
+        </div>
       </div>
     </div>
   );
