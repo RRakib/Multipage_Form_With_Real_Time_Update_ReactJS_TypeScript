c4b3fe2458dcb154c511a74ee1e6e9d9722c0667
Rakib Uddin <16103030@iubat.edu>
2019-05-22 22:33:12 +0600

    Initial Setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="multipageForm">
+        <div className="pages">Form</div>
+        <div className="pages">Real Time Update</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
